35e0adf86a9dee0444339283dc8700a48905947b
jciszewski <jciszewski@amusys.pl>
2023-04-24 20:49:52 +0200

    save

--- projekt/src/main/scala/MainWithForLoopNavigation.scala
+++ projekt/src/main/scala/MainWithForLoopNavigation.scala
@@ -50,11 +50,11 @@ object MainWithForLoopNavigation extends App {
     arr.mkString(", ")
   }
 
-  val aFileName: String = if (args.length > 1) args(1) else ".\\resources\\A.txt"
-  val yFileName: String = if (args.length > 2) args(2) else ".\\resources\\Y.txt"
-  val resultFileName: String = if (args.length > 3) args(3) else ".\\resources\\result.txt"
-  val timeoutTime = if (args.length > 4) args(4).toInt.seconds else 5.seconds
-  val eps: Double = if (args.length > 5) args(5).toDouble else 0.0001
+  val aFileName: String = if (args.length >= 1) args(0) else ".\\resources\\A.txt"
+  val yFileName: String = if (args.length >= 2) args(1) else ".\\resources\\Y.txt"
+  val resultFileName: String = if (args.length >= 3) args(2) else ".\\resources\\result.txt"
+  val timeoutTime = if (args.length >= 4) args(3).toInt.seconds else 5.seconds
+  val eps: Double = if (args.length >= 5) args(4).toDouble else 0.0001
 
   println("ARGS ARGUMENTS:")
   println("    1) - aFile path = " + aFileName)
